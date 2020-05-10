a5dac844f81356f9616f2f4ca239f11e4d7524b2
Mickey Loreti <mloreti10@gmail.com>
2020-05-09 22:20:23 -0700

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,92 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useRef, useEffect } from "react";
+import { useFirestore, useFirestoreCollectionData } from "reactfire";
+import Table from "react-bootstrap/Table";
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
+
+import "./App.css";
 
 function App() {
+  const gradeRef = useRef(null);
+  const dateRef = useRef(null);
+  const weightRef = useRef(null);
+  const gradesRef = useFirestore().collection("grades");
+  const grades = useFirestoreCollectionData(gradesRef);
+  const now = new Date();
+
+  useEffect(() => {
+    dateRef.current.valueAsDate = new Date(
+      Date.UTC(now.getFullYear(), now.getMonth(), now.getDate())
+    );
+  }, [now]);
+
+  const addGrade = (data) => {
+    gradesRef.add(data);
+  };
+
+  const onFormSubmit = (e) => {
+    e.preventDefault();
+    const { value: grade } = gradeRef.current;
+    const { value: date } = dateRef.current;
+    const { value: weight } = weightRef.current;
+
+    addGrade({
+      grade,
+      date: new Date(date),
+      weight,
+    });
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Diet</h1>
+      <div className="container">
+        <div className="table">
+          <h4>History</h4>
+          <Table responsive>
+            <thead>
+              <tr>
+                <th>Date</th>
+                <th>Weight</th>
+                <th>Grade</th>
+              </tr>
+            </thead>
+            <tbody>
+              {grades.map(({ date, grade, weight }) => (
+                <tr key={date.seconds}>
+                  <td>{date.toDate().toLocaleDateString()}</td>
+                  <td>{weight}</td>
+                  <td>{grade}</td>
+                </tr>
+              ))}
+            </tbody>
+          </Table>
+        </div>
+        <div className="form">
+          <h4>New Grade</h4>
+          <Form onSubmit={onFormSubmit}>
+            <Form.Group controlId="">
+              <Form.Label>Date</Form.Label>
+              <Form.Control type="date" ref={dateRef} required />
+            </Form.Group>
+            <Form.Group controlId="">
+              <Form.Label>Grade</Form.Label>
+              <Form.Control as="select" ref={gradeRef} required>
+                <option>A</option>
+                <option>B</option>
+                <option>C</option>
+                <option>D</option>
+                <option>F</option>
+              </Form.Control>
+            </Form.Group>
+            <Form.Group controlId="">
+              <Form.Label>Weight</Form.Label>
+              <Form.Control type="number" step=".1" ref={weightRef} required />
+            </Form.Group>
+            <Button type="submit">Submit</Button>
+          </Form>
+        </div>
+      </div>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,14 +1,34 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import { FirebaseAppProvider, SuspenseWithPerf } from "reactfire";
+
+import "./index.css";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDAcpOJhivHCS0GGD8opng4ujdXJ8_babk",
+  authDomain: "diet-school.firebaseapp.com",
+  databaseURL: "https://diet-school.firebaseio.com",
+  projectId: "diet-school",
+  storageBucket: "diet-school.appspot.com",
+  messagingSenderId: "605118797037",
+  appId: "1:605118797037:web:223a3393c646e3ea0428e4",
+};
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <FirebaseAppProvider firebaseConfig={firebaseConfig}>
+      <SuspenseWithPerf
+        fallback={<p>loading grades...</p>}
+        traceId={"load-grades-status"}
+      >
+        <App />
+      </SuspenseWithPerf>
+    </FirebaseAppProvider>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want your app to work offline and load faster, you can change
